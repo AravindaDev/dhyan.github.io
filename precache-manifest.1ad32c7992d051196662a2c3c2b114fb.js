3df98745932b0cf2258c4ee40ce1180aa133067d
dhyanarya <38901097+dhyanarya@users.noreply.github.com>
2019-09-10 16:26:53 +0530

    React Todo App

---
+++ precache-manifest.1ad32c7992d051196662a2c3c2b114fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10505dd0c7fecd73f3d54e32fc511486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0d30bbe52eb36fc702c",
+    "url": "/static/css/main.aa2b34cf.chunk.css"
+  },
+  {
+    "revision": "fcbd2043d7324f343967",
+    "url": "/static/js/2.754f28f2.chunk.js"
+  },
+  {
+    "revision": "d0d30bbe52eb36fc702c",
+    "url": "/static/js/main.5a09c49c.chunk.js"
+  },
+  {
+    "revision": "49ac1ea5616922871865",
+    "url": "/static/js/runtime~main.0735b599.js"
+  }
+]);
